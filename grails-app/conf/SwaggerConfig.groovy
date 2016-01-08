154fcf85847d4b18596b6d716b1ccfcd752b3676
Donald Jackson <donald@centio.co>
2016-01-08 17:40:38 +0200

    Upgraded to Grails 3 and upgraded Swagger to 2.0

--- grails-app/conf/SwaggerConfig.groovy
+++
@@ -1,4 +0,0 @@
-org.grails.jaxrs.url.mappings = ['/api','/api-docs']
-org.grails.jaxrs.provider.init.parameters = [
-        'com.sun.jersey.config.property.packages':
-                'com.wordnik.swagger.sample.resource;com.wordnik.swagger.jaxrs.listing']
--- src/main/groovy/swagger4jaxrs/Swagger4jaxrsGrailsPlugin.groovy
+++ src/main/groovy/swagger4jaxrs/Swagger4jaxrsGrailsPlugin.groovy
@@ -1,14 +1,14 @@
+import grails.core.GrailsApplication
+import grails.plugins.Plugin
 import grails.util.Environment
 
-import org.codehaus.groovy.grails.commons.GrailsApplication
+import io.swagger.jaxrs.config.BeanConfig
 
-import com.wordnik.swagger.jaxrs.config.BeanConfig
-
-class Swagger4jaxrsGrailsPlugin {
-    def version = "0.3-SNAPSHOT"
-    def grailsVersion = "2.0 > *"
+class Swagger4jaxrsGrailsPlugin extends Plugin {
+    def version = "0.4"
+    def grailsVersion = "3.0 > *"
     def pluginExcludes = [
-        "web-app/WEB-INF/**"
+            "web-app/WEB-INF/**"
     ]
     def title = "Swagger for JAX-RS Plugin"
     def description = 'Adds Swagger support to document REST APIs of projects that use the JAX-RS (JSR 311) plugin'
@@ -17,53 +17,54 @@ class Swagger4jaxrsGrailsPlugin {
     def license = "APACHE"
     def organization = [ name: "nerdErg Pty. Ltd.", url: "http://www.nerderg.com/" ]
     def developers = [
-        [ name: "Angel Ruiz", email: "aruizca@gmail.com" ],
-        [ name: "Aaron Brown", email: "brown.aaron.lloyd@gmail.com"],
+            [ name: "Angel Ruiz", email: "aruizca@gmail.com" ],
+            [ name: "Aaron Brown", email: "brown.aaron.lloyd@gmail.com"],
     ]
     def issueManagement = [ system: "GitHub", url: "https://github.com/nerdErg/swagger4jaxrs/issues" ]
     def scm = [ url: "https://github.com/nerdErg/swagger4jaxrs" ]
 
     def dependsOn = [
-        jaxrs: "0.11 > *"
+            jaxrs: "0.12 > *"
     ]
 
     def loadAfter = [
-        "jaxrs"
+            "jaxrs"
     ]
 
-    def doWithSpring = {
-        mergeConfig(application)
+    Closure doWithSpring() {{ ->
+        println "Configuring Swagger \n"
+        mergeConfig(grailsApplication)
 
-        ConfigObject local = application.config.'swagger4jaxrs'
+        ConfigObject local = grailsApplication.config.'swagger4jaxrs'
 
         validateLocalConfig(local)
 
         swaggerConfig(BeanConfig) { bean ->
             bean.autowire = true
             resourcePackage = local.resourcePackage
-            basePath = local.basePath ?: ''
+            basePath = local.basePath
             version = local.version ?: '1'
-            title = local.title ?: grails.util.Metadata.current.'app.name'
+            title = local.title ?: 'Unspecified'
             description = local.description ?: ''
             contact = local.contact ?: ''
             license = local.license ?: ''
             licenseUrl = local.licenseUrl ?: ''
             scan = local.scan ?: true
         }
-    }
+    }}
 
-    def onConfigChange = { event ->
-        mergeConfig(application)
+    def onConfigChange(event) {
+        mergeConfig(grailsApplication)
 
-        ConfigObject local = application.config.'swagger4jaxrs'
+        ConfigObject local = grailsApplication.config.'swagger4jaxrs'
 
         validateLocalConfig(local)
 
         event.ctx.getBean('swaggerConfig').with {
             resourcePackage = local.resourcePackage
-            basePath = local.basePath ?: '/'+grails.util.Metadata.current.'app.name'
+            basePath = local.basePath
             version = local.version ?: "1"
-            title = local.title ?: grails.util.Metadata.current.'app.name'
+            title = local.title ?: 'Unspecified'
             description = local.description ?: ""
             contact = local.contact ?: ""
             license = local.license ?: ""
@@ -77,14 +78,14 @@ class Swagger4jaxrsGrailsPlugin {
      * @param app
      */
     private void mergeConfig(GrailsApplication app) {
-        ConfigObject currentConfig = app.config.org.grails.jaxrs
-        ConfigSlurper slurper = new ConfigSlurper(Environment.current.name)
-        ConfigObject secondaryConfig = slurper.parse(app.classLoader.loadClass("SwaggerConfig"))
-
-        ConfigObject config = new ConfigObject()
-        config.putAll(secondaryConfig.org.grails.jaxrs.merge(currentConfig))
-
-        app.config.org.grails.jaxrs = config
+//        ConfigObject currentConfig = app.config.org.grails.jaxrs
+//        ConfigSlurper slurper = new ConfigSlurper(Environment.current.name)
+//        ConfigObject secondaryConfig = slurper.parse(app.classLoader.loadClass("SwaggerConfig"))
+//
+//        ConfigObject config = new ConfigObject()
+//        config.putAll(secondaryConfig.org.grails.jaxrs.merge(currentConfig))
+//
+//        app.config.org.grails.jaxrs = config
     }
 
     private void validateLocalConfig(ConfigObject local) {
